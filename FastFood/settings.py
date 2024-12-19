5e51c3d8f0614df59bae863daee8dec124612307
yadu <yadhukrishnan9751@gmail.com>
2024-12-19 12:55:10 +0530

    added requirements

--- FastFood/settings.py
+++ FastFood/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2(l==u^cc%3kl5^7()p%%^)=0qi(8(xvd*qgzse*=lds+nmmg&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
